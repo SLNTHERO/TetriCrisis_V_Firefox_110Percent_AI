fc42d4c87200cad0cd1704fe1c0d267321bb9c90
SLNTHERO <SLNTHERO@SLNTHERO.com>
2020-06-30 20:04:32 -0400

    5.0.0.1

--- README.md
+++
@@ -1,2 +0,0 @@
-# TetriCrisis_V_Firefox_110Percent_AI
-100% FREE Open-Source Cross-Platform HTML5/JavaScript Tetris Remake!
--- interface.js
+++ interface.js
@@ -1,21 +1,22 @@
 /*
- TetriCrisis V "Firefox" 110% A.I. - Puzzle game
+  TetriCrisis V "Firefox" 110% A.I. - Puzzle game
+  Copyright (C) 2020 - 16BitSoft Inc.
 
- Copyright (C) 2017  JeZxLee
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
 
- This program is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 3 of the License, or
- (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software Foundation,
- Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+  Email the author at: www.16BitSoft.com
 */
 
 // "interface.js"...
